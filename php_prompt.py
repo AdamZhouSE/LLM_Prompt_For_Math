73469adef0776b8bfb1beea4eaac712d7d82ef4e
AdamZhouSE <zhouzhuyan@gmail.com>
2024-10-16 15:01:45 +0800

    fix: hint not added

--- php_prompt.py
+++ php_prompt.py
@@ -11,19 +11,18 @@ For example, if the answer is 560, you should write #### 560."""
 class ProgressiveHint(Evaluation):
     def __init__(self, llm, prompt_method, record_path):
         super().__init__(llm, prompt_method, record_path)
-        self.hint = None
         self.max_hint = 5
 
-    def question_prompt_with_hint(self, question):
+    def question_prompt_with_hint(self, question, hint):
         prompt = f'Question: {question}'
-        if self.hint:
-            prompt = f'Question: {question}(Hint: The answer is near to {self.hint})'
+        if len(hint) > 0:
+            prompt = f'Question: {question}(Hint: The answer is near to {",".join(hint)})'
         return prompt
 
     def answer_prompt(self, answer):
         return f"Answer:\nLet's think step by step.\n{answer}"
 
-    def n_shot_chats(self, n: int, question: str):
+    def n_shot_chats(self, n: int, question: str, hint: list):
         chats = [{"role": "system", "content": system_prompt}]
 
         # for q, a in self.n_shot_list[:n]:
@@ -32,17 +31,19 @@ class ProgressiveHint(Evaluation):
         #     chats.append(
         #         {"role": "assistant", "content": self.answer_prompt(a)})
 
-        chats.append({"role": "user", "content": self.question_prompt_with_hint(question)})
+        chats.append({"role": "user", "content": self.question_prompt_with_hint(question, hint)})
         return chats
 
     def progressive_hint(self, data):
-        # generate prompt
-        prompt = self.generate_prompt(data['question'])
         total_completion_tokens = 0
         total_time = 0.0
         # chat with llm multiple times until the answer is the same
         last_llm_answer = None
+        hint = []
         for i in range(self.max_hint):
+            # generate prompt
+            prompt = self.generate_prompt_with_hint(data['question'], hint)
+            print(prompt)
             full_response = self.llm.get_full_response(prompt)
             total_completion_tokens += full_response['completion_tokens']
             total_time += full_response['time']
@@ -51,8 +52,12 @@ class ProgressiveHint(Evaluation):
             if last_llm_answer == llm_answer:
                 break
             last_llm_answer = llm_answer
+            hint.append(last_llm_answer)
         time.sleep(3)
         return last_llm_answer, total_completion_tokens, total_time
+
+    def generate_prompt_with_hint(self, question, hint):
+        return self.n_shot_chats(0, question, hint)
 
     def evaluation(self, data):
         """
